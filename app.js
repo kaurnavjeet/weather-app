3d853277d270a742d776459e680791508ecc724d
kaurnavjeet <kaurnavjeet09@gmail.com>
2023-09-15 23:59:21 -0700

    fixing css

--- app.js
+++ app.js
@@ -1,40 +1,51 @@
-let cityValue = "";
-document.addEventListener("DOMContentLoaded", () => {
-	const city = document.querySelector("input");
-	console.log(city);
+const weatherDIV = document.querySelector(".weather-container");
 
-	if (city) {
-		city.addEventListener("keydown", (event) => {
-			if (event.key === "Enter") {
-				cityValue = city.value;
-				console.log(cityValue);
-				event.preventDefault();
-			}
-		});
-	}
-});
+if (document.getElementById("city").textContent !== "City") {
+	weatherDIV.style.display = "block";
+}
 
 // connect to WeatherAPI.com
-const apiURL = `https://weatherapi-com.p.rapidapi.com/current.json?q=${cityValue}`;
+const apiURL = "https://weatherapi-com.p.rapidapi.com/current.json";
 
 const headers = {
 	"X-RapidAPI-Key": "bf9fb0cbe2mshb3ebcbecd98c7abp183eccjsncc0a3851dc4e",
 	"X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
 };
 
-async function getWeather() {
+async function getWeather(cityValue) {
 	try {
-		const res = await fetch(apiURL, {
+		const queryParams = new URLSearchParams({ q: cityValue });
+		const apiUrl = `${apiURL}?${queryParams}`;
+		const res = await fetch(apiUrl, {
 			method: "GET",
-			params: {
-				q: cityValue,
-			},
 			headers: headers,
 		});
-		console.log(res.json());
+		const data = await res.json();
+		document.getElementById("city").textContent = data.location.name;
+		document.getElementById("country").textContent = data.location.country;
+		document.getElementById("temperature").textContent =
+			data.current.temp_f + "°F";
+		document.getElementById("weather").textContent =
+			data.current.condition.text;
+		document.getElementById("humidity").textContent =
+			data.current.humidity + "%";
+
+		weatherDIV.style.display = "block";
 	} catch (err) {
 		console.log(err);
 	}
 }
 
-getWeather();
+document.addEventListener("DOMContentLoaded", () => {
+	const city = document.querySelector("input");
+	weatherDIV.style.display = "none";
+
+	if (city) {
+		city.addEventListener("keydown", (event) => {
+			if (event.key === "Enter") {
+				getWeather(city.value);
+				event.preventDefault();
+			}
+		});
+	}
+});
